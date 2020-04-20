07a04cd3f38b8bf20a30cd18f903f9ee957eb5b5
ChrisNGDC <chrisgabor95@gmail.com>
2020-04-19 22:08:23 -0300

    Initial commit

--- src/Library.hs
+++ src/Library.hs
@@ -4,24 +4,24 @@ import PdePreludat
 -- 1. Numeros
 
 siguiente :: Number -> Number
-siguiente = (+ 1)
+siguiente = (+1)
 
 esPositivo :: Number -> Bool
-esPositivo = (> 0)
+esPositivo = (>0)
 
 inversa :: Number -> Number
-inversa = (1 /)
+inversa = (1/)
 
 -- 2. Temperaturas
 
 celsiusAFarenheit :: Number -> Number
-celsiusAFarenheit = (+ 32).(* 1.8)
+celsiusAFarenheit temperatura = (temperatura * 1.8) + 32
 
 farenheitACelsius :: Number -> Number
-farenheitACelsius = (/ 1.8).(+ (-32))
+farenheitACelsius temperatura = (temperatura - 32) / 1.8
 
 haceFrioCelsius :: Number -> Bool
-haceFrioCelsius = (<= 8)
+haceFrioCelsius = (<=8)
 
 haceFrioFarenheit :: Number -> Bool
 haceFrioFarenheit = haceFrioCelsius.farenheitACelsius
@@ -29,22 +29,54 @@ haceFrioFarenheit = haceFrioCelsius.farenheitACelsius
 -- 3. Mas numeros!
 
 max' :: Number -> Number -> Number
-max' numero1 numero2
-                    |numero1 > numero2 = numero1
-                    |otherwise = numero2
+max' numero1 numero2  
+                    | numero1 > numero2 = numero1
+                    | otherwise = numero2
 
 min' :: Number -> Number -> Number
-min' numero1 numero2
-                    |numero1 < numero2 = numero1
-                    |otherwise = numero2
+min' numero1 numero2  
+                    | numero1 < numero2 = numero1
+                    | otherwise = numero2
 
 -- 4. Pinos
 
+alturaPinoCambioDePeso = 3
+pesoPinoPorCentimetroHastaCambio = 3
+pesoPinoPorCentimetroDesdeCambio = 2
+pesoUtilLimiteInferior = 400
+pesoUtilLimiteSuperior = 1000
+
+metrosACentimetros :: Number -> Number
+metrosACentimetros = (*100)
+
+pesoHastaCambio :: Number -> Number
+pesoHastaCambio altura = metrosACentimetros (min' alturaPinoCambioDePeso altura) * pesoPinoPorCentimetroHastaCambio
+
+pesoDesdeCambio :: Number -> Number
+pesoDesdeCambio altura = metrosACentimetros (max' alturaPinoCambioDePeso altura - alturaPinoCambioDePeso) * pesoPinoPorCentimetroDesdeCambio
+
 pesoPino :: Number -> Number
-pesoPino altura = (min' altura 3)* 300 + (altura - (min' altura 3)) * 200
+pesoPino altura = pesoHastaCambio altura + pesoDesdeCambio altura
 
 esPesoUtil :: Number -> Bool
-esPesoUtil peso = peso >= 400 && peso <= 1000
+esPesoUtil peso = (peso >= pesoUtilLimiteInferior) && (peso <= pesoUtilLimiteSuperior)
 
 sirvePino :: Number -> Bool
 sirvePino = esPesoUtil.pesoPino
+
+-- Cosas extras mias
+
+restoMaxMin :: Number -> Number -> Number
+restoMaxMin num1 num2 = rem (max' num1 num2) (min' num1 num2)
+
+mcd :: Number -> Number -> Number
+mcd num1 num2 
+            | restoMaxMin num1 num2 /= 0 = mcd (min' num1 num2) (restoMaxMin num1 num2)
+            | otherwise = min' num1 num2
+
+mcm :: Number -> Number -> Number
+mcm num1 num2 = (num1 / mcd num1 num2) * num2
+
+-- Ejercicios
+
+-- 1
